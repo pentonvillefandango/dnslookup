55454a520b67fc8c86400fe1228f35f60b4f1f54
pentonvillefandango <118852278+pentonvillefandango@users.noreply.github.com>
2023-02-11 16:36:15 +0000

    added output as argument instead of option

--- dnslookupparser.sh
+++ dnslookupparser.sh
@@ -7,30 +7,34 @@
 #addresses for each url. urls that do not return a authorative source IP will not
 #be in the final file
 
-# Check that that a file is provided and it is a normal file.
 
+help() {
+    echo 
+    echo "This script will convert a list of urls to a list of ip addresses"
+    echo "Usage as follows:"
+    echo
+    echo "dnslookupparser.sh [OPTIONS..] /path/to/inputfile path/to/outputfile"
+    echo
+    echo "Options"
+    echo "-h show this help"
+    echo "-v verbose mode"
+    echo
+    exit 1
+}
 
-# echo "Parameter 1: ${1}"
-# echo "Parameter 2: ${2}"
-# echo "Parameter 3: ${3}"
-# echo "Parameter 4: ${4}"
-# echo "Parameter 4: ${5}"
-
-while getopts f:v SCRIPT_OPTIONS
+while getopts hv SCRIPT_OPTIONS
 do
     case $SCRIPT_OPTIONS in
-        f)
-            echo " will output to a file"
-            FILE_OUTPUT='true'
-            FILE_NAME="${OPTARG}"
-            echo "${FILE_NAME}"
-            ;;
         v)  
-            echo " verbose mode on"
+            echo "Verbose mode on"
             VERBOSE_MODE='true'
             ;;
+        h)  help
+            ;;
         ?)
-            echo "invalid option suppplied"
+            echo "Invalid option suppplied"
+            echo 
+            echo "dnslookupparser.sh -h for help"
             exit 1
             ;;
     esac
@@ -38,16 +42,17 @@ done
 
 shift "$(( OPTIND -1 ))"
 
-# echo "Parameter 1: ${1}"
-# echo "Parameter 2: ${2}"
-# echo "Parameter 3: ${3}"
-# echo "Parameter 4: ${4}"
-# echo "Parameter 4: ${5}"
-
-if [[ ! -f ${1} ]]
+if [[ ! -f ${1} ]] || [[ ${#} -lt 2 ]]
 then
-    echo "Please provide a file containg urls"
+    echo "Please provide an input file and an output file"
+    echo
+    echo "dnslookupparser.sh -h for help"
     exit 1
+fi
+
+if [[ -f ${2} ]]
+then
+   truncate -s 0 ${2} 
 fi
 
 while read URL_LINE
@@ -57,27 +62,16 @@ do
         nslookup ${URL_LINE} &>/dev/null
         if [[ ${?} -eq 0 ]]
         then 
-            nslookup ${URL_LINE} | sed '6q;d' | awk -F ' ' '{print $2}'
+            nslookup ${URL_LINE} | sed '6q;d' | awk -F ' ' '{print $2}' >> ${2}
         fi
     else
 
         nslookup ${URL_LINE} 
         if [[ ${?} -eq 0 ]]
         then 
-            nslookup ${URL_LINE} | sed '6q;d' | awk -F ' ' '{print $2}'
+            nslookup ${URL_LINE} | sed '6q;d' | awk -F ' ' '{print $2}' >> ${2}
         fi
     fi
 done < ${1}
 
     
-# loop through the file, read each line
-
-# perform DNS lookup for each line
-
-
-# if lookup is successful then do the folllowing
-
-# remove excess lines
-# remove excess data from each line
-# write to file
-
