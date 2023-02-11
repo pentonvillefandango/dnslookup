8d7a400b2787a5407ed1b03f1529df8da8c64e18
pentonvillefandango <118852278+pentonvillefandango@users.noreply.github.com>
2023-02-11 18:05:09 +0000

    Version 1 changes

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # dnslookup
+
+## v0.1 --complete
+
+Initial commit
+
+Added check for existence of provided file
+
+## v0.2 --complete
+
+Basic Functionality
+
+## v.1.0 --complete
+
+moved replicated stuff to functions
+fixed redirect errors
+added comments
+added version number
+
+## v.1.1 WIP
--- dnslookupparser.sh
+++ dnslookupparser.sh
@@ -7,6 +7,7 @@
 #addresses for each url. urls that do not return a authorative source IP will not
 #be in the final file
 
+# help Function
 
 help() {
     echo 
@@ -17,10 +18,18 @@ help() {
     echo
     echo "Options"
     echo "-h show this help"
-    echo "-v verbose mode"
+    echo "-v verbose mode. Will show each individual DNS lookup attempt"
     echo
     exit 1
 }
+
+# mynslookup Function
+
+mynslookup() {
+    nslookup ${URL_LINE} | sed '6q;d' | awk -F ' ' '{print $2}' >> ${1}
+}
+
+# Evalute getopts 
 
 while getopts hv SCRIPT_OPTIONS
 do
@@ -34,13 +43,16 @@ do
         ?)
             echo "Invalid option suppplied"
             echo 
-            echo "dnslookupparser.sh -h for help"
-            exit 1
+            help
             ;;
     esac
 done
 
+# Remove options from arguments
+
 shift "$(( OPTIND -1 ))"
+
+# Check if input file is a real file and if output file exists
 
 if [[ ! -f ${1} ]] || [[ ${#} -lt 2 ]]
 then
@@ -50,26 +62,32 @@ then
     exit 1
 fi
 
+# if output file already exists empty it
+
 if [[ -f ${2} ]]
 then
    truncate -s 0 ${2} 
 fi
 
+# loop through lines in input file 
+
 while read URL_LINE
 do
+    # if VERBOSE_MODE is not true then send real time output to /dev/null
     if [[ $VERBOSE_MODE != 'true' ]]
     then
+        # try dns lookup in order to get exit status if 0 then execute mynslookup function 
         nslookup ${URL_LINE} &>/dev/null
         if [[ ${?} -eq 0 ]]
         then 
-            nslookup ${URL_LINE} | sed '6q;d' | awk -F ' ' '{print $2}' >> ${2}
+            mynslookup ${2}
         fi
     else
-
+        # try dns lookup in order to get exit status if 0 then execute mynslookup function 
         nslookup ${URL_LINE} 
         if [[ ${?} -eq 0 ]]
         then 
-            nslookup ${URL_LINE} | sed '6q;d' | awk -F ' ' '{print $2}' >> ${2}
+            mynslookup ${2}
         fi
     fi
 done < ${1}
